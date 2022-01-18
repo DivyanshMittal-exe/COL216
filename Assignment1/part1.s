4924737e6b8ed9a879b03b01cf7bb0c543e9f4d5
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2022-01-18 18:44:25 +0530

    Cleaner Implementation

--- Assignment1/part1.s
+++ Assignment1/part1.s
@@ -1,78 +1,72 @@
 .equ SWI_Exit, 0x11
+
+.global compare
+
 .text
 
-
 _start:
-    ldr     r0, =string1
-    ldr     r1, =string2
-    mov     r2, #1          @for comparison mode 
+    ldr     r1, =string1
+    ldr     r2, =string2
+    mov     r3, #1          @for comparison mode Put 0 for case sensitive mode, and 1 for case insensitive
 
 compare:
-    cmp     r2,#1
-    bne     comp_sens
-
-normalise:
-    mov     r5,r0
     mov     r6,r1
-1:  ldrb     r2,[r0,#0]
-    add     r0,r0,#1
-    cmp     r2, #'\n'
-    beq     2f
-    cmp     r2,#'a'
-    blt     1b
-    sub     r2,r2,#32
-    strb     r2,[r0,#-1]
-    b       1b
+    mov     r7,r2
 
-2:  ldrb     r2,[r1,#0]
-    add     r1,r1,#1
-    cmp     r2, #'\n'
-    beq     3f
-    cmp     r2,#'a'
-    blt     2b
-    sub     r2,r2,#32
-    strb     r2,[r1,#-1]
-    b       2b
-
-3:  
-    mov     r0,r5
-    mov     r1,r6
-
-
-comp_sens:
-
-    mov      r2,#0
-    ldrb     r3,[r0,#0]
+1:  mov      r0,#0
     ldrb     r4,[r1,#0]
-    cmp      r3, #'\n'
-    cmpeq    r4, #'\n'
+    ldrb     r5,[r2,#0]
+
+    cmp      r3,#0
+    beq      2f
+    bl       normalise
+    
+2:  cmp      r4, #'\n'
+    cmpeq    r5, #'\n'
     beq      last
-    mov      r2, #-1
-    cmp      r3, #'\n'
-    beq      last
-    mov      r2, #1
+    mov      r0, #-1
     cmp      r4, #'\n'
     beq      last
+    mov      r0, #1
+    cmp      r5, #'\n'
+    beq      last
 
-    mov      r2, #1
-    cmp      r3,r4
+    mov      r0,#1
+    cmp      r4,r5
     bhi      last
 
-    mov      r2, #-1
-    cmp      r3,r4
+    mov      r0, #-1
+    cmp      r4,r5
     blo      last
 
-    add     r0,r0,#1
-    add     r1,r1,#1
-    b       comp_sens
+    add      r1,r1,#1
+    add      r2,r2,#1
+    b       1b
+
+normalise:
+    cmp     r4,#'a'
+    blt     1f
+    cmp     r4,#'z'
+    bgt     1f
+    sub     r4,r4,#32
+
+1:  cmp     r5,#'a'
+    blt     2f
+    cmp     r5,#'z'
+    bgt     2f
+    sub     r5,r5,#32
+2:  mov     pc, lr
+
 
 last:
+    mov     r1,r6
+    mov     r2,r7
 
 
 swi SWI_Exit
 
 
 .data
-string1: .ascii "ADD\n"
-string2: .ascii "HelloWorld\n"
+string1: .ascii "a\n"
+string2: .ascii "A\n"
 .end
