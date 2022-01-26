fba2845914fd6ff98568e1d2820016602af6fee2
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2022-01-26 06:36:43 +0530

    Coompleted merge

--- Assignment1/part2.s
+++ Assignment1/part2.s
@@ -8,17 +8,16 @@ _start:
     ldr     r3,=arr
     ldr     r4,=arr
 
-1:  str     r0,[r3]
-    add     r3,r3,#4
-
-    mov     r1,#10
-    mov     r2,#0
-
     stmfd	sp!, {r0}
     ldr     r0,=onei
     bl      prints
     ldmfd	sp!, {r0}
 
+1:  str     r0,[r3]
+    add     r3,r3,#4
+
+    mov     r1,#10
+    mov     r2,#0
 
     bl      fgets
 
@@ -35,17 +34,16 @@ _start:
     add     r0,r0,#1
     mov     r6,r3
 
+    Cstmfd	sp!, {r0}
+    ldr     r0,=twoi
+    bl      prints
+    ldmfd	sp!, {r0}
     
 3:  str     r0,[r3]
     add     r3,r3,#4
 
     mov     r1,#10
     mov     r2,#0
-
-    stmfd	sp!, {r0}
-    ldr     r0,=twoi
-    bl      prints
-    ldmfd	sp!, {r0}
 
     bl      fgets
 
@@ -65,8 +63,36 @@ _start:
     mov     r2,r6
     mov     r3,r7
 
-    mov     r4,#0
-    mov     r5,#0
+    stmfd	sp!, {r0}
+    ldr     r0,=ins
+    bl      prints
+    ldmfd	sp!, {r0}
+
+    stmfd	sp!, {r0-r2}
+    ldr     r0,=ins_ans
+    mov     r1,#4
+    mov     r2,#0
+    bl      fgets
+    ldr     r0,=ins_ans
+    ldrb    r4, [r0,#0]
+    sub     r4, r4, #'0'
+    ldmfd	sp!, {r0-r2}
+
+    stmfd	sp!, {r0}
+    ldr     r0,=dup
+    bl      prints
+    ldmfd	sp!, {r0}   
+
+    stmfd	sp!, {r0-r2}
+    ldr     r0,=dup_ans
+    mov     r1,#4
+    mov     r2,#0
+    bl      fgets
+    ldr     r0, =dup_ans
+    ldrb    r5, [r0,#0]
+    sub     r5, r5, #'0'
+    ldmfd	sp!, {r0-r2}    
+
     bl      merge
 
 5:  cmp     r0,r1
@@ -89,8 +115,12 @@ _start:
 arr:    .space 160
 strn:   .space 400
 
-onei: .asciz   "Enter 1st List of String. Keep entering Strings with Enter, and when list one is over press CTRL + Enter \n"
-twoi: .asciz   "Enter 2nd List of String. Keep entering Strings with Enter, and when list two is over press CTRL + Enter \n"
+onei:       .asciz   "Enter 1st List of String. Keep entering Strings with Enter, and when list one is over press CTRL + Enter \n"
+twoi:       .asciz   "Enter 2nd List of String. Keep entering Strings with Enter, and when list two is over press CTRL + Enter \n"
+ins:        .asciz   "Enter 0 for case-sensitive mode and 1 for case-insensitive mode \n"
+ins_ans:    .space 4
 
+dup:        .asciz   "Enter 0 for keep duplicates and 1 to remove them \n"
+dup_ans:    .space 4
 
 .end
